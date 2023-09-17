d96d32c8e5776286f957e329399075da69da530f
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2023-09-17 15:54:27 +0530

    missing file

---
+++ template.js
@@ -0,0 +1,38 @@
+export const taskDic ={
+    "max_crawl_pages": 10,
+    "load_resources": true,
+    "enable_javascript": true,
+    "custom_js": "meta = {}; meta.url = document.URL; meta;",
+  }
+
+  export const scrDic = {
+
+  }
+
+  export const expDic ={
+    "limit": 10
+  }
+
+  export const lhrDic ={
+    "for_mobile": true,
+    "tag": "some_string_123"
+  }
+
+  export const tempDic = [
+    {
+        "max_crawl_pages": 10,
+        "load_resources": true,
+        "enable_javascript": true,
+        "custom_js": "meta = {}; meta.url = document.URL; meta;",
+      },
+      {
+
+      },
+      {
+        "limit": 10
+      },
+      {
+        "for_mobile": true,
+        "tag": "some_string_123"
+      }
+  ]
